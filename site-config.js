4ab06962acb43a954e542e70d1eaa3f1f3495332
dingyi <dingyimac@gmail.com>
2019-11-11 15:00:13 +0800

    update deps

--- site-config.js
+++ site-config.js
@@ -4,10 +4,9 @@ module.exports = {
   siteTitle: `小丁丁微信群`,
   siteTitleShort: `小丁丁`,
   siteDescription: `A Wechat group of all full-stack makers.`,
-  siteUrl: `https://ding.one/xdd`,
+  siteUrl: `https://xdd.one`,
   themeColor: `hsla(233, 32%, 56%, 1)`,
   backgroundColor: `hsla(261, 92%, 5%, 1)`,
-  pathPrefix: `/xdd`,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
     twitter: `dingyi`,
